d8f873023323ec98ce7c779ed8f64e6bd1ac78d5
Rohan Varma <rvarma@palantir.com>
2017-08-16 21:27:55 -0400

    v0 of the database done

--- backend/models.py
+++ backend/models.py
@@ -20,17 +20,31 @@ class Major(models.Model):
 	def __str__(self):
 		return self.college.name + ": " + self.name
 
+class OrReq(models.Model):
+	reqCourses = models.ManyToManyField("Course", related_name="orChildren")
+	course = models.ForeignKey("Course", related_name="orReqs")
+
+class AndReq(models.Model):
+	reqCourses = models.ManyToManyField("Course", related_name="andChildren")
+	course = models.ForeignKey("Course", related_name="andReqs")
+
 class Course(models.Model):
 	name = models.CharField(max_length=500)
 	depth = models.IntegerField(default=0)
 	college = models.ForeignKey(College, related_name="courses")
 	majors = models.ManyToManyField(Major, related_name="courses")
-	children = models.ManyToManyField("Course", related_name="parents")
 	courseNumber = models.CharField(max_length=128)
+	description = models.TextField(default="")
 
 	def __str__(self):
 		return self.college.name + ": " + self.name + ": " + str(self.depth)
 
 class CoursiconUser(AbstractUser):
 	college = models.ForeignKey(College, related_name="students", blank=True, null=True)
-	
+
+class TakenCourse(models.Model):
+	course = models.ForeignKey(Course, related_name="studentsTaken")
+	user = models.ForeignKey(CoursiconUser, related_name="takenCourses")
+	term = models.CharField(max_length=100, default="")
+	review = models.TextField(default="")
+	rating = models.IntegerField(default=0)
